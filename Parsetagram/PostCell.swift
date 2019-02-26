7edbd673d366c06f8dd4fca4216bb9c8ec9dc24f
Eros Gonzalez <ErosGonzalez@eross-mbp.lan>
2019-02-26 02:23:48 -0800

    Parsetagram final

---
+++ Parsetagram/PostCell.swift
@@ -0,0 +1,27 @@
+//
+//  PostCell.swift
+//  Parsetagram
+//
+//  Created by Eros Gonzalez on 2/26/19.
+//  Copyright © 2019 Eros Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var commentLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
